144ab299ad2fad49dc233312a9b44a9646b7b0d8
KasumiTakahashi <ec2-user@ip-172-31-36-220.ec2.internal>
2021-03-21 14:54:03 +0000

    1st-day

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '071b872fa3ef1649b60595709fbfa811dcb8b55872d4de07dd2c40d7d460ddd3f14a757be963ade7eb3d39a52203e417fa9f642cc8c47d094e4fe2f676cad129'
+
+Refile.secret_key = '962ece882698b38f1e4129308ed8f6a515b6c9084449f1088609b17fc0d19430edb0cd3e34550381427e31f202a2381c7c22d8c0f390f9e56552cc5032b04e14'
+Refile.secret_key = '7f3d21c64b96837eb744fbc8019b38fbeadf0d688789ff0297f01601a132be5676a85d042c92c221080e81b7bb454fc80d19503513185f0d339f06bf783c3261'
